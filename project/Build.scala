58a33c623816582568a71ed16bf6398c33a035be
Marko Mikulicic <marko.mikulicic@isti.cnr.it>
2012-01-31 16:39:50 +0100

    Added deps

--- project/Build.scala
+++ project/Build.scala
@@ -9,10 +9,13 @@ object ApplicationBuild extends Build {
 
     val appDependencies = Seq(
       // Add your project dependencies here,
+      "com.mongodb.casbah" %% "casbah" % "2.1.5-1",
+      "edu.cmu" % "secondstring" % "1.0.0-SNAPSHOT"
     )
 
     val main = PlayProject(appName, appVersion, appDependencies).settings(defaultScalaSettings:_*).settings(
       // Add your own project settings here      
+      resolvers += "Clojars" at "http://clojars.org/repo/"
     )
 
 }
