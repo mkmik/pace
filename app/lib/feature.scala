6e0844166eb04222b364b942ceeb29c9cc3b42f6
Marko Mikulicic <marko.mikulicic@isti.cnr.it>
2012-02-13 19:05:02 +0100

    Forgot field extractor

---
+++ app/lib/feature.scala
@@ -0,0 +1,58 @@
+package afm
+
+import com.mongodb.casbah.Imports._
+
+
+trait FeatureExtractor[A] {
+  def extract(doc: Document): Seq[A]
+}
+
+trait ValueExtractor[A] {
+  def extractValue(field: Field): Seq[A]
+}
+
+class FieldFeatureExtractor[A](val field: FieldDef[A]) extends FeatureExtractor[A] {
+  self: ValueExtractor[A] =>
+
+  def extract(doc: Document): Seq[A] = extractValue(doc.fields(field.name))
+}
+
+trait NGramValueExtractor extends ValueExtractor[String] {
+  def extractValue(field: Field): Seq[String] = {
+    field match {
+      case StringField(value) => value.sliding(3).take(64).toSeq
+      case _ => throw new Exception("unsupported field type")
+    }
+  }
+}
+
+
+class MongoFeatureExtractor[A](val extractor: FeatureExtractor[A], val fileName: String) {
+  val source = MongoConnection()("pace")("people")
+  val sink = new java.io.PrintWriter(new java.io.File(fileName))
+
+  def run {
+    run(Model.limit)
+  }
+
+  def run(limit: Option[Int]) {
+	  val rs = source.find() map MongoUtils.toDocument
+    var n = 0
+
+    def scan {
+      for(doc <- rs) {
+        if(limit match { case Some(x) => n > x; case None => false })
+          return
+
+        for(f <- extractor.extract(doc))
+          sink.println("%s:%s".format(f.toString.trim, doc.fields("n").asInstanceOf[IntField].value))
+
+        n += 1
+        if (n % 1000 == 0)
+          println("---------------------------------------- %s".format(n))
+      }
+    }
+
+    scan
+  }
+}
