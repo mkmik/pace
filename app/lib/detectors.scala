ee8759919ab5118903b910420e7a8eb2de4d3cff
Marko Mikulicic <marko.mikulicic@isti.cnr.it>
2012-02-14 12:04:55 +0100

    cleanup

--- app/lib/detectors.scala
+++ app/lib/detectors.scala
@@ -18,22 +18,22 @@ trait Detector {
 }
 
 class MongoStreamDetector(val key: String) extends Detector {
-  def run {   
-	  val rs = source.find().sort(Map(key -> 1)) map MongoUtils.toDocument
+  def run {
+    val rs = source.find().sort(Map(key -> 1)) map MongoUtils.toDocument
     Duplicates.windowedDetect(rs, collector, Model.windowSize)
   }
-} 
+}
 
 class MongoSortedHashDetector(val hashes: Int) extends Detector {
   def run {
     val collector = new MongoDBCollector("candidates")
-    
-    for(i <- 0 to hashes)  {
-      val key = "h%s".format(i)   
 
-	    val rs = source.find().sort(Map(key -> 1)) map MongoUtils.toDocument
+    for(i <- 0 to hashes)  {
+      val key = "h%s".format(i)
+
+      val rs = source.find().sort(Map(key -> 1)) map MongoUtils.toDocument
       Duplicates.windowedDetect(rs, collector, Model.windowSize)
-    } 
+    }
   }
 }
 
