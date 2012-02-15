35c3060b59295ed3d87c580bc2999d161f8e9242
Marko Mikulicic <marko.mikulicic@isti.cnr.it>
2012-02-15 18:04:59 +0100

    implemented doc from json

--- app/lib/mongo.scala
+++ app/lib/mongo.scala
@@ -16,3 +16,16 @@ object MongoUtils {
    ):_*
   ))
 }
+
+
+object JsonUtils {
+  def toDocument(rs: Map[String, Any]) = new Document(Map(
+    (for(field <- Model.fields)
+     yield (field.name, field match {
+       case IntFieldDef(name, _) => IntField(rs.get(name).getOrElse(0.0).asInstanceOf[Double].toInt)
+       case StringFieldDef(name, _) => StringField(rs.get(name).getOrElse("").asInstanceOf[String])
+       case ListFieldDef(name, _) => throw new Exception("not implemented yet")
+     })
+   ):_*
+  ))
+}
