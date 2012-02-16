eded15880b3939bd7c8cf2ed7b874e3e117509ee
Marko Mikulicic <marko.mikulicic@isti.cnr.it>
2012-02-16 10:10:27 +0100

    Extracted filenames

--- test/test.scala
+++ test/test.scala
@@ -6,23 +6,26 @@ import afm._
 
 object DbSpec extends Specification {
 
-  "the db" should {
-    "handle arrays" in {
+  "pace" should {
+    "rule" in {
+      val featuresFile = "/tmp/ngrams.txt"
+      val sortedFeaturesFile = "/tmp/ngrams.sorted"
+
 
       val features = new FieldFeatureExtractor(StringFieldDef("lastName", NullDistanceAlgo())) with NGramValueExtractor
-      val feature = new MongoFeatureExtractor(features, "/tmp/ngrams.txt")
+      val feature = new MongoFeatureExtractor(features, featuresFile)
       feature.run
 
-      val sorter = new Sorter("/tmp/ngrams.txt", "/tmp/ngrams.sorted")
+      val sorter = new Sorter(featuresFile, sortedFeaturesFile)
       sorter.run
 
       val lines = sorter.lines
 
       //val runner = new MongoStreamDetector("n", Some(lines))
-      //val runner = new MongoExternallySorted("/tmp/hashes.sorted")
-      val runner = new PrefetchingMongoExternallySorted("/tmp/ngrams.sorted", Some(lines))
-      //val runner = new ParalellFetchMongoExternallySorted("/tmp/ngrams.sorted", Some(lines))
-      //val runner = new CmdlineMongoExternallySorted("/tmp/ngrams.sorted", Some(lines))
+      //val runner = new MongoExternallySorted(sortedFeaturesFile)
+      val runner = new PrefetchingMongoExternallySorted(sortedFeaturesFile, Some(lines))
+      //val runner = new ParalellFetchMongoExternallySorted(sortedFeaturesFile, Some(lines))
+      //val runner = new CmdlineMongoExternallySorted(sortedFeaturesFile, Some(lines))
       runner.run
 
       "test" must startWith("test")
