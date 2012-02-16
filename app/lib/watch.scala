cd1e0736b3d9ac68ef4bcd76c56061c11d8d022c
Marko Mikulicic <marko.mikulicic@isti.cnr.it>
2012-02-16 14:29:44 +0100

    Timed execution of run body

---
+++ app/lib/watch.scala
@@ -0,0 +1,16 @@
+package afm
+
+object Watch {
+  def timed[A](caption: String)(body: => A) = {
+    println("executing %s".format(caption))
+    val (res, time) = timeTook(body)
+    println("executed %s in %sms".format(caption, time))
+    res
+  }
+
+  def timeTook[A](body: => A) = {
+    val start = System.currentTimeMillis
+    val res = body
+    (res, System.currentTimeMillis - start)
+  }
+}
--- test/test.scala
+++ test/test.scala
@@ -3,39 +3,46 @@ package test
 import org.specs2.mutable._
 
 import afm._
+import afm.Watch._
 
 object DbSpec extends Specification {
 
   "pace" should {
     "rule" in {
-      Model.algo match {
-        case "singleField" => {
-          val runner = new MongoStreamDetector(Model.sortOn)
-          runner.run
-        }
-        case "ngram" => {
+      val (res, time) = timeTook {
+        println("running")
+        Model.algo match {
+          case "singleField" => {
+            val runner = new MongoStreamDetector(Model.sortOn)
+            runner.run
+          }
+          case "ngram" => {
 
-          val featuresFile = "/tmp/ngrams.txt"
-          val sortedFeaturesFile = "/tmp/ngrams.sorted"
+            val featuresFile = "/tmp/ngrams.txt"
+            val sortedFeaturesFile = "/tmp/ngrams.sorted"
 
 
-          val features = new FieldFeatureExtractor(StringFieldDef("lastName", NullDistanceAlgo())) with NGramValueExtractor
-          val feature = new MongoFeatureExtractor(features, featuresFile)
-          feature.run
+            val features = new FieldFeatureExtractor(StringFieldDef("lastName", NullDistanceAlgo())) with NGramValueExtractor
+            val feature = new MongoFeatureExtractor(features, featuresFile)
+            feature.run
 
-          val sorter = new Sorter(featuresFile, sortedFeaturesFile)
-          sorter.run
+            val sorter = new Sorter(featuresFile, sortedFeaturesFile)
+            sorter.run
 
-          val lines = sorter.lines
+            val lines = sorter.lines
 
-          //val runner = new MongoStreamDetector("n", Some(lines))
-          //val runner = new MongoExternallySorted(sortedFeaturesFile)
-          val runner = new PrefetchingMongoExternallySorted(sortedFeaturesFile, Some(lines))
-          //val runner = new ParalellFetchMongoExternallySorted(sortedFeaturesFile, Some(lines))
-          //val runner = new CmdlineMongoExternallySorted(sortedFeaturesFile, Some(lines))
-          runner.run
+            //val runner = new MongoStreamDetector("n", Some(lines))
+            //val runner = new MongoExternallySorted(sortedFeaturesFile)
+            val runner = new PrefetchingMongoExternallySorted(sortedFeaturesFile, Some(lines))
+            //val runner = new ParalellFetchMongoExternallySorted(sortedFeaturesFile, Some(lines))
+            //val runner = new CmdlineMongoExternallySorted(sortedFeaturesFile, Some(lines))
+            runner.run
+          }
         }
       }
+
+      println("res %s, time %s".format(res, time))
+
       "test" must startWith("test")
     }
   }
