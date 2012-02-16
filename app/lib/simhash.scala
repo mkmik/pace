747d4b0f8057ac9ff0e2b82b30a8e505bfdf169a
Marko Mikulicic <marko.mikulicic@isti.cnr.it>
2012-02-16 23:33:25 +0100

    reimplemented simhash in a way that is faster

--- app/lib/simhash.scala
+++ app/lib/simhash.scala
@@ -7,7 +7,7 @@ import BitSetUtils._
 object Simhash {
   val bits = 32
 
-  def simhash(str: String) = {
+  def slowSimhash(str: String) = {
     val v = new Array[Int](bits)
 
     for(feature <- str.sliding(2).map(_.hashCode).map(_.toBitSet)) {
@@ -20,6 +20,24 @@ object Simhash {
       sim = sim << 1 | (if(b) 1 else 0)
     sim
   }
+
+  def simhash(str: String) = {
+    val v = new Array[Int](bits)
+
+    for(feature <- str.sliding(2).map(_.hashCode)) {
+      var n = feature
+      for(b <- 0 until bits) {
+        v(b) += (if((n & 1) == 1) 1 else 0)
+        n = n >>> 1
+      }
+    }
+
+    var sim = 0
+    for(b <- v.map(_ > 0))
+      sim = sim << 1 | (if(b) 1 else 0)
+    sim
+  }
+
 
   def rotated(i: Int): Int = rotated(i, 1)
   def rotated(i: Int, bits: Int): Int = Integer.rotateLeft(i, bits)
