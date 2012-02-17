2df1da5a3d63e157c8ffb493a5767896d8b077cb
Marko Mikulicic <marko.mikulicic@isti.cnr.it>
2012-02-17 15:49:48 +0100

    Cleanup reporting

--- test/test.scala
+++ test/test.scala
@@ -11,26 +11,33 @@ import scala.sys.runtime
 
 object DbSpec extends Specification {
 
+  def reportToCsv(metrics: (Double, Double, Int), time: Long) {
+    val (precision, recall, candidates) = metrics
+
+    val size = Model.limit match {
+      case Some(x) => x
+      case None => Model.mongoDb("people").count
+    }
+    val cores = Model.cores.getOrElse(runtime.availableProcessors)
+
+    val reportFileName = Model.conf.getString("pace.reportFile").getOrElse("/tmp/pace.csv")
+    val exists = new File(reportFileName).exists
+
+    for(report <- managed(new PrintWriter(new FileWriter(reportFileName , true)))) {
+      if(!exists)
+        report.println("size,window, cores, threshold,time,precision,recall,candidates")
+      report.println((size, Model.windowSize, cores, Model.threshold, time/1000, precision, recall, candidates).productIterator.map(_.toString).mkString(","))
+    }
+  }
+
   "pace" should {
     "rule" in {
 
       println("running")
-      val ((precision, recall, candidates), time) = timeTook { Model.scanner.run }
+      val (metrics, time) = timeTook { Model.scanner.run }
+      println("done")
 
-      val size = Model.limit match {
-        case Some(x) => x
-        case None => Model.mongoDb("people").count
-      }
-      val cores = Model.cores.getOrElse(runtime.availableProcessors)
-
-      val reportFileName = Model.conf.getString("pace.reportFile").getOrElse("/tmp/pace.csv")
-      val exists = new File(reportFileName).exists
-
-      for(report <- managed(new PrintWriter(new FileWriter(reportFileName , true)))) {
-        if(!exists)
-          report.println("size,window, cores, threshold,time,precision,recall,candidates")
-        report.println((size, Model.windowSize, cores, Model.threshold, time/1000, precision, recall, candidates).productIterator.map(_.toString).mkString(","))
-      }
+      reportToCsv(metrics, time)
 
       "test" must startWith("test")
     }
