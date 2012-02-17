3f70a68960de3d104a7b86ecea85403801b37ac1
Marko Mikulicic <marko.mikulicic@isti.cnr.it>
2012-02-17 09:13:53 +0100

    Updated graphs

--- pace-stats/graph.R
+++ pace-stats/graph.R
@@ -5,6 +5,9 @@ times1000k <- times[times$size == 1000000,]
 times10000k <- times[times$size == 10000000,]
 
 
+timesSize <- times[times$window==100,]
+
+
 p100k <- ggplot(times100k, aes(window), ylabel="") + 
   geom_line(aes(y = precision, linetype="precision"))  +
   geom_line(aes(y = recall, linetype = "recall")) + scale_y_continuous("",limits = c(0, 1)) +
@@ -35,8 +38,8 @@ ggsave(filename="/tmp/t100k.pdf", height=4, width=7)
 t1000k <- ggplot(times1000k, aes(window)) + geom_line(aes(y = time))
 ggsave(filename="/tmp/t1000k.pdf", height=4, width=7)
 
-sizes <- read.table("sizes.csv",sep=",",header=TRUE)
-s <- ggplot(sizes, aes(size)) + geom_line(aes(y = time)) + scale_x_log10() + scale_y_log10()
+#sizes <- read.table("sizes.csv",sep=",",header=TRUE)
+s <- ggplot(timesSize, aes(size)) + geom_line(aes(y = time)) + scale_x_log10() + scale_y_log10()
 
 ggsave(filename="/tmp/sizes.pdf", height=4, width=7)
 
