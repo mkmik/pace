f45ea3661a696d69475a56b59c63e37b5826a37e
Marko Mikulicic <marko.mikulicic@isti.cnr.it>
2012-02-17 08:41:24 +0100

    updated graphs

--- pace-stats/graph.R
+++ pace-stats/graph.R
@@ -2,6 +2,8 @@ times <- read.table("times.csv",sep=",", header=TRUE)
 
 times100k <- times[times$size == 100000,]
 times1000k <- times[times$size == 1000000,]
+times10000k <- times[times$size == 10000000,]
+
 
 p100k <- ggplot(times100k, aes(window), ylabel="") + 
   geom_line(aes(y = precision, linetype="precision"))  +
@@ -17,6 +19,14 @@ p1000k <-  ggplot(times1000k, aes(window)) +
   labs(linetype = "") 
 
 ggsave(filename="/tmp/p1000k.pdf", height=4, width=7)
+
+
+p10000k <-  ggplot(times10000k, aes(window)) + 
+  geom_line(aes(y = precision, linetype = "precision")) + 
+  geom_line(aes(y = recall, linetype = "recall")) + scale_y_continuous("", limits = c(0, 1)) +
+  labs(linetype = "") 
+
+ggsave(filename="/tmp/p10000k.pdf", height=4, width=7)
 
 
 t100k <- ggplot(times100k, aes(window)) + geom_line(aes(y = time))
@@ -40,32 +50,3 @@ c <- ggplot(cores, aes(cores)) + geom_line(aes(y = speedup))
 speedup <- c
 
 ggsave(filename="/tmp/speedup.pdf", height=4, width=7)
-
-
-hashes1000k <- read.table("hashes-1000k.csv",sep=",", header=TRUE)
-
-ph1000k <-  ggplot(hashes1000k, aes(window)) + 
-  geom_line(aes(y = precision, linetype = "precision")) + 
-  geom_line(aes(y = recall, linetype = "recall")) + scale_y_continuous("", limits = c(0, 1)) +
-  labs(linetype = "") 
-
-ggsave(filename="/tmp/ph1000k.pdf", height=4, width=7)
-
-th1000k <- ggplot(hashes1000k, aes(window)) + geom_line(aes(y = time))
-
-ggsave(filename="/tmp/th1000k.pdf", height=4, width=7)
-
-
-shashes1000k <- read.table("shashes-1000k.csv",sep=",", header=TRUE)
-
-psh1000k <-  ggplot(shashes1000k, aes(window)) + 
-  geom_line(aes(y = precision, linetype = "precision")) + 
-  geom_line(aes(y = recall, linetype = "recall")) + scale_y_continuous("", limits = c(0, 1)) +
-  labs(linetype = "") 
-
-ggsave(filename="/tmp/psh1000k.pdf", height=4, width=7)
-
-tsh1000k <- ggplot(shashes1000k, aes(window)) + geom_line(aes(y = time))
-
-ggsave(filename="/tmp/tsh1000k.pdf", height=4, width=7)
-
