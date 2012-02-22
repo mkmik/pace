5c33dc654bd558b371704c092a9d0751983cc356
Claudio Atzori <claudio.atzori@isti.cnr.it>
2012-02-22 18:23:00 +0100

    added driver model definition (needs to be extendend) implemented adapter for xml files

--- app/lib/dnet.scala
+++ app/lib/dnet.scala
@@ -4,15 +4,31 @@ import afm._
 import afm.io._
 import afm.model._
 import afm.mongo._
+import scala.xml.{Elem, Node}
 import com.mongodb.casbah.Imports._
 
 
 class DNetMongoDBAdapter(implicit config: Config) extends Adapter[DBObject] {
   val xmlAdapter = new XmlAdapter
 
-  def toDocument(record: DBObject): Document = xmlAdapter.toDocument(record.getAs[String]("body").get)
+  def toDocument(record: DBObject): Document = xmlAdapter.toDocument(record.getAs[Elem]("body").get)
 }
 
-class XmlAdapter(implicit config: Config) extends Adapter[String] {
-  def toDocument(xml: String): Document = throw new Exception("not implemented yet")
+class XmlAdapter(implicit config: Config) extends Adapter[Elem] {
+
+  def toDocument(xml: Elem): Document = new MapDocument(Map(
+    (for(field <- config.fields)
+     yield (field.name, field match {
+       case IntFieldDef(name, _) => IntField(1)
+       case StringFieldDef(name, _) => StringField(select(xml, name))
+       case ListFieldDef(name, _) => ListField(List(StringField(xml.toString)))
+     })
+   ):_*
+  ))
+
+  def select(node: Elem, name: String): String = {
+    val Array(ns, fieldName) = name.split("_")
+    (node \\ fieldName).filter({k: Node => k.prefix == ns}).text
+  }
+
 }
