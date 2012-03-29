677d285951a69c73b1207de48b76229e4f832d8b
Marko Mikulicic <marko.mikulicic@isti.cnr.it>
2012-03-29 18:32:39 +0200

    Add twitter bootstrap

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -6,7 +6,9 @@ import play.api.mvc._
 object Application extends Controller {
   
   def index = Action {
-    Ok(views.html.index("Your new application is ready. Figata"))
+    Ok(views.html.index("Your new application is ready. Figata .. Mah"))
   }
+
+  def test = TODO
   
 }
