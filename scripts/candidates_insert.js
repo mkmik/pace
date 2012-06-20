7a308358626e920acd556f4e2bda9b38089ab899
Marko Mikulicic <marko.mikulicic@isti.cnr.it>
2012-06-20 17:08:53 +0200

    Also handle merging projects

--- scripts/candidates_insert.js
+++ scripts/candidates_insert.js
@@ -1,5 +1,14 @@
+// the "candidates" mongodb collection contains groups of ids which are meant to be merged in one 
+
+// This code generates an auxiliary table which generates
 print("BEGIN;");
-print("TRUNCATE merges_results;");
+print("DROP TABLE IF EXISTS merges_results;");
+print("CREATE TABLE merges_results (merged_id text REFERENCES results (resultid), merges_id text REFERENCES results (resultid));");
+print("ALTER TABLE merges_results ADD unique (merged_id, merges_id);");
+print("CREATE OR REPLACE FUNCTION cleanup_results_projects_frommerge() RETURNS void LANGUAGE plpgsql AS  $$ DECLARE BEGIN delete from results_projects where frommerge = true;  END; $$;");
+print("SELECT cleanup_results_projects_frommerge");
+print("ALTER TABLE results_projects DROP IF EXISTS frommerge;");
+print("ALTER TABLE results_projects ADD frommerge bool DEFAULT false;");
 db.candidates.find({}, {'ids': 1}).forEach(function(rec) {
     var ids = rec["ids"].sort().reverse();
     var h = ids[0];
@@ -9,4 +18,5 @@ db.candidates.find({}, {'ids': 1}).forEach(function(rec) {
 });
 print("update results set merged = 'f' where merged = 't';");
 print("update results set merged = 't' from merges_results m where m.merges_id = resultid;");
+print("INSERT INTO results_projects (result,project,frommerge) SELECT m.merged_id,rp.project,'t' FROM merges_results m JOIN results_projects rp on (m.merges_id = rp.result) WHERE project not in (SELECT project FROM results_projects rp2 WHERE rp2.result = m.merged_id);");
 print("COMMIT;");
