71a8a7ff4bd2fdf67c672564a6e3b3db046cf9b2
Marko Mikulicic <marko.mikulicic@isti.cnr.it>
2012-12-11 17:00:01 +0100

    Back to single project, it's difficult to keep up with a multi-project sbt module, who depends on too much stuff that doesn't keep up with scala versions (for example because sbteclipse cannot generate scala-ide definitions when even a single dependency in a side-project isn't met)

--- project/Build.scala
+++ project/Build.scala
@@ -3,33 +3,6 @@ import Keys._
 
 object ApplicationBuild extends Build {
 
-  val appName         = "pace-web"
-  val appVersion      = "1.0"
-
-  val appDependencies = Seq(
-    // Add your project dependencies here,
-    "com.mongodb.casbah" %% "casbah" % "2.1.5-1",
-    "edu.cmu" % "secondstring" % "1.0.0-SNAPSHOT",
-    "com.google.inject" % "guice" % "3.0",
-    //"com.google.inject.extensions" % "guice-servlet" % "3.0",
-    "uk.me.lings" % "scala-guice_2.8.0" % "0.1",
-
-    "joda-time" % "joda-time" % "2.0",
-    "org.joda" % "joda-convert" % "1.2",
-    "postgresql" % "postgresql" % "8.4-701.jdbc4",
-    "com.traveas" % "querulous-light_2.9.0" % "0.0.6",
-
-    "com.jsuereth" %% "scala-arm" % "1.2",
-
-    "net.sf.opencsv" % "opencsv" % "2.1",
-
-    "com.typesafe.config" % "config" % "0.2.1",
-
-    "com.typesafe.akka" % "akka-remote" % "2.0.3",
-
-    "org.scalatest" % "scalatest" % "1.2"
-  )
-
   val libDependencies = Seq(
     // Add your project dependencies here,
     "edu.cmu" % "secondstring" % "1.0.0-SNAPSHOT",
@@ -59,49 +32,10 @@ object ApplicationBuild extends Build {
     "org.json" % "json" % "20090211"
   )
 
-
-  val libDbDependencies = libDependencies ++ Seq(
-    "postgresql" % "postgresql" % "8.4-701.jdbc4",
-    "com.traveas" % "querulous-light_2.9.0" % "0.0.6"
-  )
-
-  val libMongoDependencies = libDependencies ++ Seq(
-    "com.mongodb.casbah" %% "casbah" % "2.1.5-1"
-  )
-
-  val paceLib = Project("pace", file("modules/pace")).settings(
+  lazy val root = Project("pace", file(".")).settings(
     libraryDependencies := libDependencies,
     resolvers += "Clojars" at "http://clojars.org/repo/",
     resolvers += "RI Releases" at "http://maven.research-infrastructures.eu/nexus/content/repositories/releases",
     resolvers += "Typesafe" at "http://repo.typesafe.com/typesafe/releases/"
   )
-
-  val paceLibDb = Project("pace-db", file("modules/pacedb")).settings(
-    libraryDependencies := libDbDependencies,
-    resolvers += "Clojars" at "http://clojars.org/repo/",
-    resolvers += "RI Releases" at "http://maven.research-infrastructures.eu/nexus/content/repositories/releases"
-  ).dependsOn(paceLib)
-
-  val paceLibMongo = Project("pace-mongo", file("modules/pacemongo")).settings(
-    libraryDependencies := libMongoDependencies,
-    resolvers += "Clojars" at "http://clojars.org/repo/",
-    resolvers += "RI Releases" at "http://maven.research-infrastructures.eu/nexus/content/repositories/releases"
-  ).dependsOn(paceLib)
-
-
-  val main = Project(appName, file(".")).settings(
-    libraryDependencies := appDependencies,
-    // Add your own project settings here
-    resolvers += "Clojars" at "http://clojars.org/repo/",
-    resolvers += "RI Releases" at "http://maven.research-infrastructures.eu/nexus/content/repositories/releases"
-  ).dependsOn(paceLib, paceLibDb, paceLibMongo)
-
-
-/*
-  val main = PlayProject(appName, appVersion, appDependencies).settings(defaultScalaSettings:_*).settings(
-    // Add your own project settings here
-    resolvers += "Clojars" at "http://clojars.org/repo/",
-    resolvers += "RI Releases" at "http://maven.research-infrastructures.eu/nexus/content/repositories/releases"
-  ).dependsOn(paceLib, paceLibDb, paceLibMongo)
-*/
 }
